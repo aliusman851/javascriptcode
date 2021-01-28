467e809ef25763cf4f094bea6b91c889b361ccb6
alius <alius@LAPTOP-NNR66SG1>
2021-01-28 23:21:13 +0100

    create one delete button and give the attribute to buuton.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 "use strict";
 let postId = 0;
-let main = document.querySelector("section");
+let section = document.querySelector("section");
 let addBtn = document.querySelector('#addpost');
 let blogsCountInput = document.querySelector('#counter');
 let blogsCount = 0;
@@ -8,28 +8,29 @@ blogsCountInput.value = blogsCount;
 function createblog() {
     postId++;
     let article = document.createElement('article');
-    article.classList.add('blog');
     let header = document.createElement('header');
-    header.classList.add('header');
     let h2 = document.createElement('h2');
-    h2.classList.add('post-title');
+    
+    
     h2.innerHTML = 'Titel';
+    
     header.append(h2);
+    
     article.append(header);
     let p = document.createElement('p');
-    p.classList.add('post-body');
     p.innerHTML = 'you can write your blog here ';
     article.append(p);
     let btn = document.createElement("button");
     btn.innerHTML = "delete"
-    btn.setAttribute("style", "background-color: green;");
+    
+    
     article.append(btn);
      return article;
 }
 addBtn.addEventListener('click', function() {
-    main.prepend(createblog());
+    section.prepend(createblog());
     blogsCountInput.value = ++blogsCount;
-    updatePostList();
+    
 });
 
 
