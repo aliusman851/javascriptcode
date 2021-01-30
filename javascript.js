8d0fdbf82056ed6639ca666ed09c2956babec467
alius <alius@LAPTOP-NNR66SG1>
2021-01-30 20:34:08 +0100

    create del fucntion and button

--- javascript.js
+++ javascript.js
@@ -1,37 +1,49 @@
 "use strict";
-let postId = 0;
+let id = 0;
+let string = '';
+let Array = [];
 let section = document.querySelector("section");
 let addBtn = document.querySelector('#addpost');
-let blogsCountInput = document.querySelector('#counter');
-let blogsCount = 0;
-blogsCountInput.value = blogsCount;
+let delBtn = document.querySelector('#remove');
+let counter = document.querySelector('#counter');
+let count = 0;
+counter.value = count;
 function createblog() {
-    postId++;
+    id++;
     let article = document.createElement('article');
+    article.setAttribute("class", 'blog-post');
     let header = document.createElement('header');
+    header.setAttribute("class", 'post-header');
     let h2 = document.createElement('h2');
-    
-    
+    h2.setAttribute("class", 'post-title');
+    h2.setAttribute('id', id);
     h2.innerHTML = 'Titel';
-    
-    header.append(h2);
-    
-    article.append(header);
+    header.appendChild(h2);
+    article.appendChild(header);
     let p = document.createElement('p');
-    p.innerHTML = 'you can write your blog here ';
-    article.append(p);
-    let btn = document.createElement("button");
-    btn.innerHTML = "delete"
-    
-    
-    article.append(btn);
-     return article;
+    p.setAttribute("class", 'post-body');
+    p.innerHTML = 'write your blog here';
+    article.appendChild(p);
+    let button = document.createElement("button");
+    button.innerHTML = ("delete");
+    button.setAttribute("class", "delete");
+    button.setAttribute("onclick", "deletepost()");
+    article.appendChild(button);
+
+    return article;
 }
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
     section.prepend(createblog());
-    blogsCountInput.value = ++blogsCount;
-    
+    counter.value = ++count;
+
 });
+function deletepost() {
+    if (count >= 1) {
+        counter.value = --count;
+        section.removeChild(section.children[0]);
+    }
+    else {
+        console.log(" no post to delete");
+    }
 
-
-
+}
